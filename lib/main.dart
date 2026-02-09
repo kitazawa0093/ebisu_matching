3b51081f9ebb96702a56de64a1acfd4edfe68ceb
ryo <kitazawa0093@gmail.com>
2026-02-09 21:15:03 +0900

    Local Web fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:amuse_booking/screens/login_screen.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
@@ -7,7 +8,9 @@ import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Stripe.publishableKey = 'pk_test_51SN9S71rWFNVgrh2g2BNuUrJ4fU6n3LRU1bbVb4k8moH6bRvns6Yka6kLxRXZGNHaBMairjmrp2PwzhVVu62Hcre00X4P4zHOU';
   await Stripe.instance.applySettings(); // ★追加
   runApp(const MyApp());
